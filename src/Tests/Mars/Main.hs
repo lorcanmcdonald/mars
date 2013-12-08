a822feb4581f09a72d24f5f24d79ca32b448506b
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2013-12-08 21:47:57 +0000

    Moved the test for modify doc to a unit test

--- src/Tests/Mars/Main.hs
+++ src/Tests/Mars/Main.hs
@@ -1,5 +1,5 @@
 import Data.Aeson
-import Data.Vector (singleton)
+import Data.Vector (singleton, fromList)
 import qualified Data.Text as Text
 import Debug.Trace
 import Mars.Command
@@ -22,13 +22,14 @@ queryProperties = testGroup "Query Tests"
                 [ testProperty "command parse unparse" prop_command_parse
                 , testProperty "query parse unparse" prop_query_parse
                 , testProperty "move up shortens" prop_move_up_shorten
-                , testProperty "modifyDoc modifies Document" prop_modifyDoc_modifies
             ]
 
 unitTests :: TestTree
 unitTests = testGroup "Unit Tests"
           [ testCase "Can query array" $
             queryDoc (Array . singleton . String . Text.pack $ "1") (Query [IndexedItem 0]) @?= [String . Text.pack $ "1"]
+          , testCase "Modify document" $
+            modifyDoc (Array (fromList [Number 1, Number 2, Number 3])) (Query [IndexedItem 2]) (Number 4) @?= Array (fromList [Number 1, Number 2, Number 4])
           ]
 
 prop_command_parse :: Command -> Bool
@@ -46,11 +47,4 @@ prop_move_up_shorten :: Query -> Bool
 prop_move_up_shorten q = len(moveUp q) <= len q
     where
         len (Query l) = length l
-
-prop_modifyDoc_modifies doc q new = and [isCollection doc] ==> (queryDoc updatedDoc q == [new])
-            where
-                updatedDoc = modifyDoc doc q new
-                isCollection (Array _)  = True
-                isCollection (Object _) = True
-                isCollection _          = False
 
