1d2cc42679e90f8f02610062c6bb99e4caeba502
Lorcan McDonald <lorcan@lorcanmcdonald.com>
2020-09-25 11:35:41 +0100

    Hlint suggestions

--- src/Mars/Eval.hs
+++ src/Mars/Eval.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE DoAndIfThenElse #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TupleSections #-}
 
 module Mars.Eval
   ( Output (..),
@@ -16,27 +15,16 @@ import Control.Applicative
 
 import Data.Text.IO (putStrLn)
 -- import Mars.Instances ()
-import Mars.Types
+
 import Mars.Command
+import Mars.Types
 import System.IO hiding (putStrLn)
 import Prelude hiding (putStrLn)
 
 run :: Command a => MarsState -> a -> IO MarsState
-run state command = exec . evalCommand state $ command
+run state = exec . evalCommand state
 
 exec :: (MarsState, Output) -> IO MarsState
 exec (newState, Output output) = do
   putStrLn output
   return newState
-
--- run s (Cd query) = exec $ cd s query
--- run s (Load filename) = load s filename
--- run s (Ls query) = do
---   putStrLn . toS . show $ query
---   exec $ ls s query
--- run s (Save filename) = save s filename
--- run s (Update query value) = exec $ update s query value
--- run s Pwd = exec $ pwd s
-
--- getDocument :: MarsState -> Value
--- getDocument s = fromMaybe (object []) $ document s
